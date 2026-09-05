02052fa53622ac385607ac309531393762fd99bd
reeesadh <rheasadhwani@outlook.com>
2026-09-05 13:51:58 -0700

    button!!

--- script.js
+++ script.js
@@ -2,9 +2,29 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     console.log("Received message from popup:", message);
 
     if (message.cmd === "highlight") {
+        const fontStuff = document.createElement('link');
+        fontStuff.href = "https://fonts.googleapis.com/css2?family=Aldrich&display=swap";
+        fontStuff.rel = "stylesheet";
+        document.head.appendChild(fontStuff);
         const tweets = document.querySelectorAll(["article[data-testid='tweet']"]);
-        tweets[0].style.backgroundColor = '#FF0000';
-        sendResponse({ reply: "Daughter :sob: :sob: :sob:"});
+        tweets.forEach(tweet => {
+            tweet.style.position = "relative";
+            sendResponse({ reply: "Daughter :sob: :sob: :sob:"});
+            const secretButton = document.createElement('div');
+            secretButton.style.position = "absolute";
+            secretButton.textContent = "?";
+            secretButton.style.fontSize = "24px";
+            secretButton.style.fontFamily = "Aldrich";
+            secretButton.style.fontWeight = "bold";
+            secretButton.style.color = "#1f27cc";
+            secretButton.style.top = "1%";
+            secretButton.style.left = "85%";
+            secretButton.addEventListener('click', () => {
+                alert("say wallahi bro");
+            });
+            tweet.appendChild(secretButton);
+        });
+      
     }
 
     return true; 
